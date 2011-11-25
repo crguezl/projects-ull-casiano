70b383f9ef41f8c412b393a8d53793573d17f927
Casiano Rodriguez-Leon <casiano.rodriguez.leon@gmail.com>
2011-11-25 11:40:19 +0000

    added tests and git ignore file

---
+++ test/tc_01_smoke.rb
@@ -0,0 +1,51 @@
+require 'test/unit'
+require "html"
+
+class TestHTML < Test::Unit::TestCase
+  def test_simplepage
+    q= HTML.new {  
+      html {
+        head(:dir => "chazam", :lang => "spanish") { title "My wonderful home page" }
+        body do
+          h1 "Welcome to my home page!", :class => "chuchu", :lang => "spanish"
+          b "My hobbies:"
+          ul do
+            li "Juggling"
+            li "Knitting"
+            li { i "Sleeping" } 
+            li "Metaprogramming"
+          end #ul
+        end # body
+      }
+    }
+
+    expected = <<'EXPECTEDHTMLPAGE'
+<html>
+<head dir = "chazam" lang = "spanish">
+<title>
+My wonderful home page
+</title>
+</head> <body>
+<h1 class = "chuchu" lang = "spanish">
+Welcome to my home page!
+</h1> <b>
+My hobbies:
+</b> <ul>
+<li>
+Juggling
+</li> <li>
+Knitting
+</li> <li>
+<i>
+Sleeping
+</i>
+</li> <li>
+Metaprogramming
+</li>
+</ul>
+</body>
+</html>
+EXPECTEDHTMLPAGE
+    assert_equal q.to_s.gsub(/\s+/,''), expected.gsub(/\s+/,'')
+  end
+end
