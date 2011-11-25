1339a66341e4c2316d520c37014d4bc86c9a59ef
Casiano Rodriguez-Leon <casiano.rodriguez.leon@gmail.com>
2011-11-25 10:39:06 +0000

    added html.rb

---
+++ lib/html.rb
@@ -0,0 +1,51 @@
+class HTML
+  attr_accessor :page
+
+  def initialize(&block)
+    @page = [[]]
+    instance_eval &block
+  end
+
+  def build_attr(attributes)
+      return '' if attributes.nil? or  attributes.empty?
+      attributes.inject("") { |s,x| s += %{ #{x[0]} = "#{x[1]}"} } 
+  end
+
+  def method_missing(tag, *args)
+    if block_given?
+      @page.push []
+      yield 
+      text = @page.pop.join(' ')
+      tagattr = build_attr(args.shift) 
+    else
+      text = args.shift.to_s || "\n"
+      tagattr = build_attr(args.shift) 
+    end
+    text = "<#{tag}#{tagattr}>\n#{text}\n</#{tag}>"
+    @page[-1].push text
+    text
+  end
+
+  def to_s
+    @page.join("\n")
+  end
+end
+
+if __FILE__ == $0
+  q= HTML.new {  
+    html {
+      head(:dir => "chazam", :lang => "spanish") { title "My wonderful home page" }
+      body do
+        h1 "Welcome to my home page!", :class => "chuchu", :lang => "spanish"
+        b "My hobbies:"
+        ul do
+          li "Juggling"
+          li "Knitting"
+          li { i "Sleeping" } 
+          li "Metaprogramming"
+        end #ul
+      end # body
+    }
+  }
+  puts q
+end
