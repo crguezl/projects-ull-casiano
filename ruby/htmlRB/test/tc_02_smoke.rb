17c104d4f33bce7f98198f14849a6c3f71e40f1b
Casiano Rodriguez-Leon <casiano.rodriguez.leon@gmail.com>
2011-11-29 12:38:10 +0000

    mm

--- ruby/htmlRB/test/tc_02_smoke.rb
+++ ruby/htmlRB/test/tc_02_smoke.rb
@@ -13,4 +13,27 @@ class TestHTML < Test::Unit::TestCase
     assert_match  /dir = "chazam"/, qs
     assert_match  /lang = "spanish"/, qs
   end
+
+  def test_attr2
+    q= HTML.new {  
+        html {
+          head(:dir => "chazam", :lang => "spanish") { title "My wonderful home page" }
+          body do
+            h1 "Welcome to my home page!", :class => "chuchu", :lang => "senegalese"
+            b "My hobbies:"
+            ul do
+              li "Juggling"
+              li "Knitting"
+              li { i "Sleeping" } 
+              li "Metaprogramming"
+            end #ul
+          end # body
+        }
+    }
+    qs = q.to_s
+    assert_match  /dir = "chazam"/, qs
+    assert_match  /lang = "spanish"/, qs
+    assert_match  /class = "chuchu"/, qs
+    assert_match  /lang = "senegalese"/, qs
+  end
 end
