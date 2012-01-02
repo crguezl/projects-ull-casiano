145de6259ecbea50ba451ba9e349de2b62ebb631
Casiano Rodriguez-Leon <casiano.rodriguez.leon@gmail.com>
2012-01-02 10:13:56 +0000

    m

---
+++ ruby/DRb/helloWorld/helloworldclient2.rb
@@ -0,0 +1,11 @@
+require 'drb'
+
+#                      "druby://127.0.0.1:61676"
+# druby://imac-de-casiano-rodriguez-leon.local:49357
+DRb.start_service
+
+#address = "druby://imac-de-casiano-rodriguez-leon.local:61676"
+address = "druby://127.0.0.1:61676"
+server = DRbObject.new_with_uri(address)
+
+puts server.say_hello
