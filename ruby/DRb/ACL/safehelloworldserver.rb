9e85b4125e7b5f5441204d3149b4f750e18b892e
Casiano Rodriguez <casiano.rodriguez.leon@gmail.com>
2012-01-09 15:13:26 +0000

    m

--- ruby/DRb/ACL/safehelloworldserver.rb
+++ ruby/DRb/ACL/safehelloworldserver.rb
@@ -10,7 +10,10 @@ $SAFE = 1
 File.open('DRbhw.proc', 'w') do |f|
   f.puts $$
 end
-address = "druby://imac-de-casiano-rodriguez-leon.local:61676"
+#address = "druby://imac-de-casiano-rodriguez-leon.local:61676"
+
+serverip = File.open('MyIP', 'r').gets.chomp
+address = "druby://#{serverip}:61676"
 DRb.start_service(address, obj=HelloWorldServer.new)
 puts "Process #{$$}: Server running at #{address} serving #{obj}"
 DRb.thread.join
