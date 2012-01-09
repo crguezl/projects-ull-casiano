95e950d46d9371370c9d5ffa5fd9cb04ebd60663
Casiano Rodriguez <casiano.rodriguez.leon@gmail.com>
2012-01-09 15:06:56 +0000

    m

--- ruby/DRb/ACL/helloworldserver2.rb
+++ ruby/DRb/ACL/helloworldserver2.rb
@@ -6,7 +6,12 @@ class HelloWorldServer
   end
 end
 
+File.open('DRbhw.proc', 'w') { |f| f.puts $$ }
+
+serverip = File.open('MyIP', 'r').gets.chomp
+
 #address = "druby://imac-de-casiano-rodriguez-leon.local:61676"
-address = "druby://127.0.0.1:61676"
+#address = "druby://127.0.0.1:61676"
+address = "druby://#{serverip}:61676"
 DRb.start_service(address, HelloWorldServer.new)
 DRb.thread.join
