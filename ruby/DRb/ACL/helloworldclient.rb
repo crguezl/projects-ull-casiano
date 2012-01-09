f1e435694714f7b1102cb6443dc165671d09c04c
Programacion en Paralelo II <pp2@nereida.deioc.ull.es>
2012-01-09 11:33:11 +0000

    m

--- ruby/DRb/ACL/helloworldclient.rb
+++ ruby/DRb/ACL/helloworldclient.rb
@@ -5,7 +5,9 @@ require 'drb'
 DRb.start_service
 
 #address = "druby://imac-de-casiano-rodriguez-leon.local:61676"
-myip = `ifconfig en0 inet`.match(/(?:(?:\d{,3}\.)+\d{,3})/)[0]
+myip = ''
+File.open('MyIP', 'r') { |f| myip = f.gets.chomp! }
+puts "<#{myip}>"
 address = "druby://#{myip}:61676"
 server = DRbObject.new_with_uri(address)
 
