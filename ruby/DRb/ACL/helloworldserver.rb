bbf55ca294cb1f8736d37461317e37e3c5bb324b
Programacion en Paralelo II <pp2@nereida.deioc.ull.es>
2012-01-09 12:26:28 +0000

    m

--- ruby/DRb/ACL/helloworldserver.rb
+++ ruby/DRb/ACL/helloworldserver.rb
@@ -10,13 +10,16 @@ end
 
 File.open('DRbhw.proc', 'w') { |f| f.puts $$ }
 
-myip = ''
-File.open('MyIP', 'r') { |f| myip = f.gets.chomp! }
-puts "<#{myip}>"
-acl = ACL.new(%w{deny all allow }+ [ myip ])
+myip = File.open('MyIP', 'r').readlines.map { |ip| ["allow",  ip.chomp] }.flatten
+serverip = myip[1]
+
+puts "<#{myip.inspect}>"
+
+acl = ACL.new(%w{deny all }+ myip)
+puts acl.inspect
 DRb.install_acl(acl)
 
-address = "druby://#{myip}:61676"
+address = "druby://#{serverip}:61676"
 puts address
 DRb.start_service(address, obj=HelloWorldServer.new)
 puts "Process #{$$}: Server running at #{address} serving #{obj}"
