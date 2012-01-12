0c60738130819a2f3411bffb2f939bf71b324023
Casiano Rodriguez <casiano.rodriguez.leon@gmail.com>
2012-01-12 12:02:50 +0000

    m

--- ruby/DRb/SSH/shared_queue_server.rb
+++ ruby/DRb/SSH/shared_queue_server.rb
@@ -4,5 +4,5 @@ require "thread"
 File.open('DRbhw.proc', 'w') { |f| f.puts $$ }
 
 queue = SizedQueue.new(10)
-DRb.start_service('druby://localhost:9001', queue)
+DRb.start_service('druby://localhost:9000', queue)
 DRb.thread.join
