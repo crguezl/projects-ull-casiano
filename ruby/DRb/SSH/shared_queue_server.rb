a696530946d5946674f3ac29ccd527cacd989ec0
PP2 <casiano.rodriguez.leon@gmail.com>
2012-01-13 12:35:36 +0000

    m

--- ruby/DRb/SSH/shared_queue_server.rb
+++ ruby/DRb/SSH/shared_queue_server.rb
@@ -1,7 +1,7 @@
 require "drb/drb"
 require "thread"
 
-File.open('DRbSSH/DRbhw.proc', 'w') { |f| f.puts $$ }
+File.open("#{ENV['HOME']}/DRbSSH/DRbhw.proc", 'w') { |f| f.puts $$ }
 
 queue = SizedQueue.new(5)
 DRb.start_service('druby://localhost:61676', queue)
