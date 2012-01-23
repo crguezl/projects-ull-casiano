f9b9062958898df8abff4f9062c31a02ed411269
Casiano Rodriguez <casiano.rodriguez.leon@gmail.com>
2012-01-23 16:18:54 +0000

    m

---
+++ DotCloud/helloworldapp2/src/index.php
@@ -0,0 +1,16 @@
+<?php
+
+# Import environment settings from DotCloud
+$envjson = json_decode(file_get_contents("/home/dotcloud/environment.json"),true);
+
+# Create MySQL Connection
+$mysqli = new mysqli($envjson['DOTCLOUD_DB_MYSQL_HOST'],
+                     'helloappuser',         # username
+                     'helloworldpassword',   # password
+                     'helloworldapp2',       # db name
+                     $envjson['DOTCLOUD_DB_MYSQL_PORT']);
+
+print_r($mysqli->query('SELECT now();'));
+
+?>
+
